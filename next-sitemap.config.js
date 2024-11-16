c92edaad0ae79f325dda6c46dfead18f2634078e
siebentod <33163165+siebentod@users.noreply.github.com>
2024-11-16 12:08:20 +0300

    smallfix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://rigveda.vercel.app/',
+  siteUrl: 'https://rigveda-hymns.vercel.app',
   generateRobotsTxt: true,
   exclude: ['/about'],
 };
